939cc6114619a249607f8912cd3c20b1107ea27b
Radosław Piórkowski <rp335451@students.mimuw.edu.pl>
2016-01-13 23:14:38 +0100

    Fix offset calculation

--- src/Backend/X86/Runner.hs
+++ src/Backend/X86/Runner.hs
@@ -19,25 +19,22 @@ import Backend.X86.Transformations.InitialisationOfVariables
 
 genASM :: Program -> IO String
 genASM p1 = do
-  liftIO $ putStrLn "[ 0/3 ] Compilation."
+  liftIO $ putStrLn "[ 0/2 ] Compilation."
 
   -- Preprocessing of program tree
   let p2 = initializeAllVariables p1
-  let p3 = unifyVariables p2
+  p3    <- unifyVariables p2
 
-  liftIO $ putStrLn "[ 1/3 ] Compilation."
+  liftIO $ putStrLn "[ 1/2 ] Compilation."
   liftIO $ putStrLn $ indent 8 $ printWhite $ "Transformend program tree:"
   liftIO $ putStrLn $ indent 8 $ printGreen $ printTree p3
 
   -- Compilation
   st <- execStateT (emitProgram p3) initialState
 
-  liftIO $ putStrLn "[ 2/3 ] Compilation."
-
-  -- Postprocessing of instructions
   let stmts    = map printGasm $ reverse $ st ^. emittedStmts
   let preamble = map printGasm $ reverse $ st ^. preambleStmts
 
-  liftIO $ putStrLn "[ 3/3 ] Compilation."
+  liftIO $ putStrLn "[ 2/2 ] Compilation."
 
   return $ concat preamble ++ concat stmts
--- src/Backend/X86/Transformations/UnifyVariables.hs
+++ src/Backend/X86/Transformations/UnifyVariables.hs
@@ -3,7 +3,6 @@ module Backend.X86.Transformations.UnifyVariables where
 
 import Prelude hiding ( id )
 import Control.Monad.State
-import Control.Monad.Identity
 import Control.Lens hiding ( op, Empty )
 import qualified Data.Map as M
 
@@ -16,10 +15,10 @@ import Frontend.Parser.AbsLatte
 
 type IdxEnv = M.Map Ident Integer
 type UnifyState = (IdxEnv, Integer)
-type UnifyM a = StateT UnifyState Identity a
+type UnifyM a = StateT UnifyState IO a
 
-unifyVariables :: Program -> Program
-unifyVariables x = runIdentity (evalStateT (unifyVariables' x) (M.empty, 0))
+unifyVariables :: Program -> IO Program
+unifyVariables x = evalStateT (unifyVariables' x) (M.empty, 0)
 
 idxEnv :: Lens' UnifyState IdxEnv
 idxEnv = lens (fst) (\(_, b) a -> (a, b))
@@ -44,9 +43,17 @@ item2ident :: Item -> Ident
 item2ident (Init ident _) = ident
 item2ident (NoInit ident) = ident
 
+processItem :: Item -> UnifyM Item
+processItem (Init ident e1) = do
+  e1' <- unifyVariables' e1
+  return (Init ident e1')
+processItem x = return x
+
 getUniqueIdent :: Ident -> UnifyM Ident
 getUniqueIdent ident@(Ident str) = do
+  liftIO $ putStrLn $ "Before"
   idx <- uses idxEnv (M.!ident)
+  liftIO $ putStrLn $ "After"
   return (Ident $ str ++ "_#" ++ show idx)
 
 unifyVariables' :: Tree a -> UnifyM (Tree a)
@@ -59,15 +66,16 @@ unifyVariables' x = case x of
     return $ Arg t ident'
   BStmt _ -> newScope $ do
     composOpM unifyVariables' x
-  Decl _ items -> do
-    mapM_ declVar (map item2ident items)
-    composOpM unifyVariables' x
+  Decl q items -> do
+    items' <- mapM processItem items
+    mapM_ declVar (map item2ident items')
+    composOpM unifyVariables' (Decl q items')
   NoInit ident   -> do
     ident' <- getUniqueIdent ident
     return $ NoInit ident'
   Init   ident e1 -> do
     ident' <- getUniqueIdent ident
-    return $ Init ident' e1
+    return $ Init ident' e1 -- e1 was already processed in Decl
   LVar ident -> do
     ident' <- getUniqueIdent ident
     return $ LVar ident'
